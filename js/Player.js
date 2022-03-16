5f6d5f3afc80b2f4aec852b5c3e71e185199fa05
hiffaz <90331652+hiffaz@users.noreply.github.com>
2022-03-16 20:24:44 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,15 @@
+class player{
+
+
+    constructor() {
+
+
+    }
+
+   display () {
+
+
+   }
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,19 @@
+// ----------------------- starts from 3/16/2022 ------------- // 
+var backgroundImage;
+var redGhost, greenGhost, yellowGhost
+var player;
+function preload() {
+  backgroundImage = loadImage("assets/ground.png");
+
+
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(backgroundImage);
+
+}
